52cd9bf6b052cc498a1bbc5e75bfb7415b2239e1
Kate Larkins <110925353+KateLarkins@users.noreply.github.com>
2024-03-18 13:41:03 -0500

    3/18/24
    redo

--- script.js
+++ script.js
@@ -103,3 +103,16 @@ new StickyNavigation();
 //     let scrollPosition = window.pageYOffset;
 //     parallax.style.transform = 'translateY(' + scrollPosition * 0.5 + 'px)';
 // });
+// navbar scroll
+
+$(document).ready(function() {
+	// Transition effect for navbar 
+	$(window).scroll(function() {
+	  // checks if window is scrolled more than 500px, adds/removes solid class
+	  if($(this).scrollTop() > 500) { 
+		  $('.navbar').addClass('solid');
+	  } else {
+		  $('.navbar').removeClass('solid');
+	  }
+	});
+});
