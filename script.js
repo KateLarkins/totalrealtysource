b5f110b5748f627afe9a7b83e1309052b6391a90
Kate Larkins <110925353+KateLarkins@users.noreply.github.com>
2024-04-02 21:24:15 -0500

    4/2/24

--- script.js
+++ script.js
@@ -6,3 +6,24 @@ window.addEventListener('scroll', function() {
       navbar.classList.remove('navbar-scrolled');
     }
   });
+
+  // JavaScript for Navbar Collapse Toggle
+document.addEventListener('DOMContentLoaded', function () {
+  const navbarInner = document.querySelector('.navbar-inner');
+  const navbarNav = document.querySelector('.navbar-nav');
+  const navbarBars = document.querySelector('.fa-bars');
+
+  navbarBars.addEventListener('click', function () {
+    navbarInner.classList.toggle('collapsed');
+    navbarNav.classList.toggle('collapsed');
+  });
+
+  // Close navbar if a nav link is clicked
+  const navbarLinks = document.querySelectorAll('.navbar-nav li a');
+  navbarLinks.forEach(function (link) {
+    link.addEventListener('click', function () {
+      navbarInner.classList.add('collapsed');
+      navbarNav.classList.add('collapsed');
+    });
+  });
+});
