f115aae355afe5add86979256ac19c24d9531200
Kate Larkins <110925353+KateLarkins@users.noreply.github.com>
2024-04-29 12:20:39 -0500

    navbar

--- script.js
+++ script.js
@@ -73,3 +73,13 @@ window.addEventListener("resize", function() {
   }
 });
 
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
