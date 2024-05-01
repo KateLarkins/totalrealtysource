460e08d417c08531ec73547813060d2af0c06402
Kate Larkins <110925353+KateLarkins@users.noreply.github.com>
2024-05-01 08:45:37 -0500

    Updated Property Buttons

--- script.js
+++ script.js
@@ -54,3 +54,18 @@ window.addEventListener("resize", function() {
     navbar.classList.remove("responsive"); // Remove the responsive class
   }
 });
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+
+
+
+
+
