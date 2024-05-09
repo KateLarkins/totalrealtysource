2735665aa81f5fdaf67e646f4d8f3fd3137e7d52
Kate Larkins <110925353+KateLarkins@users.noreply.github.com>
2024-05-08 19:14:48 -0500

    check this again

--- script.js
+++ script.js
@@ -54,3 +54,11 @@ window.addEventListener("resize", function() {
     navbar.classList.remove("responsive"); // Remove the responsive class
   }
 });
+function toggleMenu() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
